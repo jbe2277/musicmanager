f5c8e977bba85c72439973f27d7daecf96068db0
jbe2277 <jbe2277@users.noreply.github.com>
2016-03-01 22:17:42 +0100

    Prepare the 1.4.0.100 release

--- src/MusicManager/SharedAssemblyInfo.cs
+++ src/MusicManager/SharedAssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Resources;
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
 
 
-[assembly: AssemblyVersion("1.4.0.1")]
-[assembly: AssemblyFileVersion("1.4.0.1")]
+[assembly: AssemblyVersion("1.4.0.100")]
+[assembly: AssemblyFileVersion("1.4.0.100")]
