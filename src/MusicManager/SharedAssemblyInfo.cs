567a3c7890d6c5b298ebfd798a13e34c4c481dd6
jbe2277 <jbe2277@users.noreply.github.com>
2018-11-30 10:41:33 +0100

    Prepare the 2.0.0.100 release

--- src/MusicManager/SharedAssemblyInfo.cs
+++ src/MusicManager/SharedAssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Resources;
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
 
 
-[assembly: AssemblyVersion("1.7.2.100")]
-[assembly: AssemblyFileVersion("1.7.2.100")]
+[assembly: AssemblyVersion("2.0.0.100")]
+[assembly: AssemblyFileVersion("2.0.0.100")]
