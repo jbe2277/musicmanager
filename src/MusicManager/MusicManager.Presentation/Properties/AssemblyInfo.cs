8fb4cf7c2f21e85a2f7fe41b68fbcc28dbe1ab38
jbe2277 <jbe2277@users.noreply.github.com>
2023-11-22 21:47:45 +0100

    Move InternalsVisibleTo declarations to project files

--- src/MusicManager/MusicManager.Presentation/Properties/AssemblyInfo.cs
+++ src/MusicManager/MusicManager.Presentation/Properties/AssemblyInfo.cs
@@ -1,6 +1,3 @@
-﻿using System.Runtime.CompilerServices;
-using System.Windows;
+﻿using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-
-[assembly: InternalsVisibleTo("Test.MusicManager.Presentation")]
